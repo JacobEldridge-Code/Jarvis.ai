290b5d0e2780cd51ca39076d65ee3d1564896092
donnykun <donnellcsmith@gmail.com>
2024-03-12 20:58:14 -0400

    donny's commit. searchbar

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// searchbar border
+
+let searchBar = document.getElementById("search-input");
+let searchBorder = document.querySelector(".searchbar-wrapper");
+
+searchBar.addEventListener("focus" , () => {
+    searchBorder.classList.add("focused");
+})
+
+searchBar.addEventListener("blur" , () => {
+    searchBorder.classList.remove("focused");
+});
+
+//searchbar border ends
+
+let searchButton = document.querySelector(".middle-btn");
+let searchMenu = document.getElementById("hidden-search-menu");
+
+searchButton.addEventListener("click" , () => {
+    searchButton.classList.toggle("active")
+    searchMenu.classList.toggle("active")
+    
+})
+
