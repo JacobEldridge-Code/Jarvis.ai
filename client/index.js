efd4127e9fa2dea5cd773752287ccf85ec3a95a7
Jacob Eldridge <eldridgejacob15@yahoo.com>
2024-04-10 20:32:47 -0700

    added a background color for the header once you scroll 80px

--- client/index.js
+++ client/index.js
@@ -60,3 +60,14 @@ document.addEventListener("DOMContentLoaded", function() {
 //     }
 
 // });
+
+let className = "inverted";
+let scrollTrigger = 80;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+};
